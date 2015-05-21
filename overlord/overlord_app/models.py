5f25379e12c7adc56be4ff30fe5645a7470acb97
Ryan Thompson <rthomps1@ball.com>
2015-05-21 09:34:10 -0400

    Added temp models, and created an superuser username: admin password: password

--- overlord/overlord_app/models.py
+++ overlord/overlord_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Feature(models.Model):
+    feature_name = models.CharField(max_length=200)
+    discover_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    feature = models.ForeignKey(Feature)
+    feature_text = models.CharField(max_length=200)
+    lat = models.IntegerField(default=0)
+    lon = models.IntegerField(default=0)
